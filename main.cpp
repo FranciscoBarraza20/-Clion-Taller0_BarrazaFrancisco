9150c65eef73c432b75d8ae319c77a44a9b17048
FranciscoBarraza20 <francisco.barraza@alumnos.ucn.cl>
2025-09-10 14:22:06 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,9 @@
 #include "servicios/SistemaAplicacion.h"
 #include "vista/VistaConsola.h"
 
-using namespace std;
-
 int main() {
-
     SistemaAplicacion* parteTrasera = new SistemaAplicacion();
     VistaConsola* parteFrontal = new VistaConsola(parteTrasera);
     parteFrontal->iniciar();
-
     return 0;
-
 }
